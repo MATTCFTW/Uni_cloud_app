69afa628bc50d9c699fd5e1606559f494554db52
MATTCFTW <mattcftw@gmail.com>
2021-10-19 18:24:37 +0000

    Created Initial app template

--- main.py
+++ main.py
@@ -1,10 +1,34 @@
-from flask import Flask, render_template
+import logging
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
-def index():    
-    return render_template("index.html")
+@app.route('/index')
+def index():
+ return render_template('index.html')
 
+@app.route('/products')
+def products():
+ return render_template('products.html')
+
+@app.route('/register')
+def register():
+ return render_template('register.html')
+
+@app.route('/login')
+def login():
+ return render_template('login.html')
+
+@app.errorhandler(500)
+def server_error(e):
+ # Log the error and stacktrace.
+ logging.exception('An error occurred during a request.')
+ return 'An internal error occurred.', 500
+@app.errorhandler(404)
+def page_not_found(error):
+ return render_template('404.html'), 404
+ 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=8080, debug=True) 
+# Only run for local development.
+ app.run(host='127.0.0.1', port=8080, debug=True) 
