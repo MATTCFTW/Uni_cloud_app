fdacd716d23d6bf6c5dba97277ffb9682080fa8a
MATTCFTW <mattcftw@gmail.com>
2021-12-15 12:20:03 +0000

    more python comments

--- main.py
+++ main.py
@@ -15,7 +15,9 @@ req = PreparedRequest()
 #MongoDB connection
 cluster=MongoClient( "mongodb+srv://Admin:eR4xVrLSpXr7Pecn@assignment.xtiqh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
 db=cluster["Assignment"]
+#furniture collection
 collection=db["Furniture"]
+#user orders collection
 order_collection=db["Orders"]
 
 app = Flask(__name__)
@@ -24,15 +26,20 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/index')
 def index():
+    #renders the html from the templates folder
     return render_template('index.html')
 
+#route where users can browse products
 @app.route('/products')
 def products():
     return render_template('products.html')
 
+#used when a user clicks to order a product
 @app.route('/products/clicked', methods=['POST'])
 def make_order():
+    #retrieve the json that was sent to the route using AJAX
     get_data = request.json
+    #adds it to the collection containing user orders
     order_collection.insert_one(get_data)
     response = "Item ordered"
     return response
@@ -45,8 +52,11 @@ def orders():
 #JSON of orders for logged in user
 @app.route('/orders/list', methods=["GET"])
 def user_orders():
+    #cookie that identifies the user that is logged in
     user = request.cookies.get("user")
+    #cloud function URL
     url = "https://europe-west2-assignment-328920.cloudfunctions.net/user_orders"
+    #The user identifier is used as a URL parameter
     param = { 'user': user}
     req.prepare_url(url, param)
     response = requests.get(req.url)
@@ -56,13 +66,14 @@ def user_orders():
 @app.route('/orders/delete', methods=['POST'])
 def delete_order():
     delete_data = request.json
+    #deletes one of the matching orders
     order_collection.delete_one(delete_data)
     response = "Item removed"
     return response
 
 @app.route('/account')
 def login():
-    # Verify Firebase auth.
+    #Verify Firebase auth.
     id_token = request.cookies.get("token")
     error_message = None
     claims = None
@@ -70,8 +81,8 @@ def login():
         try: 
             claims = google.oauth2.id_token.verify_firebase_token(id_token, firebase_request_adapter)
         except ValueError as exc:
-            # This will be raised if the token is expired or any other
-            # verification checks fail.
+            #This will be raised if the token is expired or any other
+            #verification checks fail.
             error_message = str(exc)           
     return render_template('account.html',  user_data=claims, error_message=error_message) 
 
@@ -79,6 +90,7 @@ def login():
 #JSON of all furniture
 @app.route("/furniture", methods=["GET"])
 def mongodbdisplay():
+    #cloud function that implements a service mesh
     url = "https://europe-west2-assignment-328920.cloudfunctions.net/Service_Mesh_Layer_Function"
     response = requests.get(url)
     return(response.content) 
